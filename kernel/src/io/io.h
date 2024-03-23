1026e9cb05bdf82bccceda0002c64871039bd84b
Jan Duinkerken <jan.duinkerken.rodriguez@gmail.com>
2024-03-23 22:54:14 +0100

    Format files

--- kernel/src/io/io.h
+++ kernel/src/io/io.h
@@ -8,7 +8,7 @@ void outl(uint16_t port, uint32_t value);
 uint8_t inb(uint16_t);
 uint16_t inw(uint16_t port);
 uint32_t inl(uint16_t port);
-void insw(uint16_t port, uint8_t* buffer, int count);
+void insw(uint16_t port, uint8_t *buffer, int count);
 void outsw(uint16_t port, uint8_t *buffer, int count);
 void io_wait();
 #endif
